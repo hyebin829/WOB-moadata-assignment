bf05c4c8264fdebfa0f56a108daec9fa6f644a79
Jihyuk Ma <t90laser93@gmail.com>
2022-05-31 16:15:20 +0900

    FEATURE: 버튼 컴포넌트 추가
    -

--- src/routes/_components/Button/index.tsx
+++ src/routes/_components/Button/index.tsx
@@ -1,7 +1,47 @@
-import styles from './button.module.scss'
+interface IButtonProps {
+  type?: 'button' | 'submit'
+  margin?: string
+  text?: string
+  width?: string
+  height?: string
+  fontSize?: string
+  border?: string
+  borderRadius?: string
+  color?: string
+  backgroundColor?: string
+  onClick?: () => void
+}
 
-const Button = () => {
-  return <div className={styles.container}>Button</div>
+const Button = ({
+  type,
+  margin,
+  text,
+  width,
+  height,
+  fontSize,
+  border,
+  borderRadius,
+  color,
+  backgroundColor,
+  onClick,
+}: IButtonProps) => {
+  const buttonStyles = {
+    text,
+    margin,
+    width,
+    height,
+    fontSize,
+    border,
+    color,
+    borderRadius,
+    backgroundColor,
+  }
+
+  return (
+    <button type={type === 'button' ? 'button' : 'submit'} style={buttonStyles} onClick={onClick}>
+      {text}
+    </button>
+  )
 }
 
 export default Button
