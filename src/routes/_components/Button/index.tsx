1aed59f41ab67e5b7bbb71a50b425f6e26c9b005
Jihyuk Ma <t90laser93@gmail.com>
2022-05-31 20:22:13 +0900

    FIX: Button 컴포넌트 타입 'button'으로 고정
    -

--- src/routes/_components/Button/index.tsx
+++ src/routes/_components/Button/index.tsx
@@ -4,17 +4,15 @@ import cx from 'classnames'
 import styles from './button.module.scss'
 
 interface IButtonProps {
-  type: 'button' | 'submit' | 'reset'
   children: ReactNode
   size: 'small' | 'large'
   primary?: boolean
   onClick?: () => void
 }
 
-export const Button = ({ type, children, size, primary, onClick }: IButtonProps) => {
+export const Button = ({ children, size, primary, onClick }: IButtonProps) => {
   return (
-    // eslint-disable-next-line react/button-has-type
-    <button type={type} className={cx(styles.button, styles[size], { [styles.primary]: primary })} onClick={onClick}>
+    <button type='button' className={cx(styles.button, styles[size], { [styles.primary]: primary })} onClick={onClick}>
       {children}
     </button>
   )
