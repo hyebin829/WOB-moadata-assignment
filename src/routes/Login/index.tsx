ed888607d44755e8810f0d283724b269acb2680a
hyebin829 <80376561+hyebin829@users.noreply.github.com>
2022-06-01 20:22:20 +0900

    Feature/login page (#8)
    * ADD : types/store
    
    * FEAT : login
    
    * MOD : input checkbox
    
    * FEAT : validation check
    
    * Refactor : handlesubmit
    
    * Refactor: checkValidation map -> forEach로 수정
    
    * Refactor: 변수명 수정

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,15 +1,60 @@
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import store from 'store'
+
+import { checkValidation } from 'services/user'
+
 import styles from './login.module.scss'
-import { useState } from 'react'
 import loginBackgroundImg from '../../assets/images/loginBackgroundImg.jpg'
 import { FaEye, FaEyeSlash, FaUser, FaTrash } from 'react-icons/fa'
 
 const UserManage = () => {
-  const [showPassword, setShowPassword] = useState(false)
+  const [adminId, setAdminId] = useState('')
   const [password, setPassword] = useState('')
-  const [userId, setUserId] = useState('')
+
+  const [isVisible, setIsVisible] = useState(false)
+  const [isError, setIsError] = useState(false)
+  const [isChecked, setIsChecked] = useState(false)
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (sessionStorage.getItem('admin') || store.get('admin')) {
+      navigate('/')
+    }
+  }, [navigate])
+
+  const handleIdInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setAdminId(e.currentTarget.value)
+    setIsError(false)
+  }
+
+  const handlePasswordInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value)
+    setIsError(false)
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (checkValidation(adminId, password)) {
+      isChecked ? store.set('admin', adminId) : store.remove('admin')
+      sessionStorage.setItem('admin', adminId)
+      navigate('/')
+    } else {
+      setIsError(true)
+    }
+  }
+
+  const handleCheck = () => {
+    setIsChecked((prev) => !prev)
+  }
+
+  const closeMessage = () => {
+    setIsError(false)
+  }
 
   const toggleVisiblePw = () => {
-    setShowPassword(!showPassword)
+    setIsVisible((prev) => !prev)
   }
 
   return (
@@ -17,36 +62,21 @@ const UserManage = () => {
       <img src={loginBackgroundImg} alt='login Background Img' />
       <div className={styles.loginBoxContainer}>
         <h1 className={styles.title}>Hello Again!</h1>
-        <form
-          action=''
-          method='POST'
-          onSubmit={(e) => {
-            e.preventDefault()
-          }}
-        >
+        <form action='submit' method='POST' onSubmit={handleSubmit}>
           <div className={styles.inputBox}>
-            <input
-              value={userId}
-              type='text'
-              name='userId'
-              onChange={(e) => {
-                setUserId(e.target.value)
-              }}
-            />
             <label htmlFor='userId'>ID</label>
+            <input value={adminId} type='text' name='userId' onChange={handleIdInput} />
             <FaUser className={styles.faUserIcon} />
           </div>
           <div className={styles.inputBox}>
+            <label htmlFor='password'>password</label>
             <input
               value={password}
-              type={showPassword ? 'text' : 'password'}
+              type={isVisible ? 'text' : 'password'}
               name='password'
-              onChange={(e) => {
-                setPassword(e.target.value)
-              }}
+              onChange={handlePasswordInput}
             />
-            <label htmlFor='password'>password</label>
-            {showPassword ? (
+            {isVisible ? (
               <FaEye className={styles.faEyeIcon} onClick={toggleVisiblePw} />
             ) : (
               <FaEyeSlash className={styles.faEyeIcon} onClick={toggleVisiblePw} />
@@ -54,23 +84,23 @@ const UserManage = () => {
           </div>
           <div className={styles.extraFeaturesWrapper}>
             <label>
-              <input type='checkbox' /> Remember Me
+              <input type='checkbox' checked={isChecked} onChange={handleCheck} /> Remember Me
             </label>
-            <span className={styles.recoveryPassword}>Recovery ID / Password</span>
           </div>
-          {/* class명 hidden 또는 floatingMsg */}
-          <div className={styles.floatingMsg}>Wrong password or ID. Try again</div>
+          {isError && <div className={styles.floatingMsg}>Wrong password or ID. Try again</div>}
           <button className={styles.loginBtn} type='submit'>
             login
           </button>
         </form>
       </div>
-      <div className={styles.notificationPopUpContainer}>
-        {/* 팝업메시지 컴포넌트화 시켜서 로그인 실패시 로그인 화면에서 해당 팝업메시지 fade in x클릭시 out, 로그인 성공시 홈화면에서, 로그아웃시 로그인화면에서 구현하면 될듯싶네요 */}
-        <div className={styles.popUpMsg}>
-          Wrong password or ID. Try again <FaTrash className={styles.handlePopUpIcon} />
+      {isError && (
+        <div className={styles.notificationPopUpContainer}>
+          {/* 팝업메시지 컴포넌트화 시켜서 로그인 실패시 로그인 화면에서 해당 팝업메시지 fade in x클릭시 out, 로그인 성공시 홈화면에서, 로그아웃시 로그인화면에서 구현하면 될듯싶네요 */}
+          <div className={styles.popUpMsg}>
+            Wrong password or ID. Try again <FaTrash className={styles.handlePopUpIcon} onClick={closeMessage} />
+          </div>
         </div>
-      </div>
+      )}
       {/*
       <div className={styles.notificationPopUpContainer}>
         <div className={styles.popUpMsg}>login succeeded</div>
