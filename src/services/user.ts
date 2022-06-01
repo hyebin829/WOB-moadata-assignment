1a6cd4f9537afb797fd28d186092a4bad99048c2
hyebin829 <smilerain779@gmail.com>
2022-06-01 17:36:46 +0900

    FEAT : validation check

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,8 +1,22 @@
 import users from 'assets/jsons/user/member.json'
+import adminData from '../assets/jsons/user/admin.json'
 
 const getMemberSeq = (id: string) => {
   const findUser = users.find((value) => value.user_id === id)
   return findUser?.member_seq
 }
 
-export { getMemberSeq }
+const checkValidation = (id: string, password: string) => {
+  const idArray = adminData.map((item) => item.user_id)
+  const passwordArray = adminData.map((item) => item.password)
+
+  let isValid = false
+  idArray.map((item, index) => {
+    if (item === id && password === passwordArray[index]) {
+      isValid = true
+    }
+  })
+  return isValid
+}
+
+export { getMemberSeq, checkValidation }
