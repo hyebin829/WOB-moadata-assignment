b6a2431dc789a7ea0eb1ccb3733d626aeff32608
hyebin829 <smilerain779@gmail.com>
2022-06-01 10:56:53 +0900

    temp

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,7 +1,53 @@
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react'
 import styles from './login.module.scss'
+import { checkValidation } from 'services/user'
+import store from 'store'
+import { useNavigate } from 'react-router-dom'
 
-const UserManage = () => {
-  return <div className={styles.container}>UserManage</div>
+const LoginPage = () => {
+  const [id, setId] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState(false)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (store.get('admin')) {
+      navigate('/')
+    }
+  }, [navigate])
+
+  const handleIdInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setId(e.currentTarget.value)
+    setErrorMessage(false)
+  }
+
+  const handlePasswordInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value)
+    setErrorMessage(false)
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (checkValidation(id, password)) {
+      store.set('admin', id)
+      navigate('/')
+    } else {
+      setErrorMessage(true)
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <form action='submit' onSubmit={handleSubmit}>
+        <label htmlFor='adminId' />
+        <input type='text' id='adminId' value={id} onChange={handleIdInput} />
+        <label htmlFor='password' />
+        <input type='password' id='password' value={password} onChange={handlePasswordInput} />
+        <button type='submit'>로그인</button>
+        {errorMessage && <div>ID 또는 PW가 다릅니다. </div>}
+      </form>
+    </div>
+  )
 }
 
-export default UserManage
+export default LoginPage
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,1 +1,16 @@
-export {}
+// export {}
+
+import adminData from '../assets/jsons/user/admin.json'
+
+export const checkValidation = (id: string, password: string) => {
+  const idArray = adminData.map((item) => item.user_id)
+  const passwordArray = adminData.map((item) => item.password)
+
+  let isValid = false
+  idArray.map((item, index) => {
+    if (item === id && password === passwordArray[index]) {
+      isValid = true
+    }
+  })
+  return isValid
+}
