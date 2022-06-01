623a7f962fbc82941bdf8c1a849ab77a96503344
김학률 <52916848+markyul@users.noreply.github.com>
2022-06-01 23:36:45 +0900

    ADD: 회원 상세 정보 레이아웃만 구현 (#14)

--- src/routes/UserInfo/index.tsx
+++ src/routes/UserInfo/index.tsx
@@ -1,7 +1,39 @@
 import styles from './userInfo.module.scss'
 
 const UserInfo = () => {
-  return <div className={styles.container}>UserInfo</div>
+  return (
+    <div className={styles.container}>
+      <h1>회원 상세 정보</h1>
+      <section className={styles.infoContainer}>
+        <dl>
+          <div>
+            <dt>로그인 ID</dt>
+            <dd>member136</dd>
+          </div>
+          <div>
+            <dt>회원 번호</dt>
+            <dd>136</dd>
+          </div>
+          <div>
+            <dt>가입일</dt>
+            <dd>2020-04-16</dd>
+          </div>
+          <div>
+            <dt>닉네임</dt>
+            <dd>모아데이터1</dd>
+          </div>
+          <div>
+            <dt>성별</dt>
+            <dd>남</dd>
+          </div>
+          <div>
+            <dt>생년월일</dt>
+            <dd>1998-02-22</dd>
+          </div>
+        </dl>
+      </section>
+    </div>
+  )
 }
 
 export default UserInfo
