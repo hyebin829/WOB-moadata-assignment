004183fc25a42aff618d8d0938aee7f955a318c6
hyebin829 <smilerain779@gmail.com>
2022-06-03 00:55:31 +0900

    FEAT : 로그인되지 않은 상태일 경우 로그인 페이지로 이동

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 
 import LoginPage from './Login'
 import HomePage from './Home'
@@ -6,17 +6,25 @@ import UserManage from './UserManage'
 import UserInfo from './UserInfo'
 import NotFoundPage from './NotFoundPage'
 import Layout from './Layout'
+import { useEffect } from 'react'
 
 const App = () => {
+  const isLogin = !!sessionStorage.getItem('admin')
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isLogin) navigate('login')
+  }, [isLogin, navigate])
+
   return (
     <Routes>
+      <Route path='login' element={<LoginPage />} />
       <Route path='/' element={<Layout />}>
         <Route path='' element={<HomePage />} />
         <Route path='userManage' element={<UserManage />} />
         <Route path='userInfo' element={<UserInfo />} />
         <Route path='*' element={<NotFoundPage />} />
       </Route>
-      <Route path='login' element={<LoginPage />} />
     </Routes>
   )
 }
